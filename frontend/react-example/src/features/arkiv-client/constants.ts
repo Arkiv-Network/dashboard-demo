6b35c62d7825870d2511b985c079d994b093e408
Seweryn Kras <seweryn.kras@golem.network>
2026-03-06 14:14:30 +0100

    change default network to kaolin

--- frontend/react-example/src/features/arkiv-client/constants.ts
+++ frontend/react-example/src/features/arkiv-client/constants.ts
@@ -1,8 +1,8 @@
 import { kaolin } from "@arkiv-network/sdk/chains";
 
-export const ARKIV_CHAIN = kaolin
-
-export const ARKIV_EXPLORER_BASE_URL = "https://explorer.infurademo.hoodi.arkiv.network";
+export const ARKIV_CHAIN = kaolin;
+export const ARKIV_EXPLORER_BASE_URL =
+  "https://explorer.kaolin.hoodi.arkiv.network";
 
 export const ENTITY_OWNER = "0xF46E23f6a6F6336D4C64D5D1c95599bF77a536f0";
 
